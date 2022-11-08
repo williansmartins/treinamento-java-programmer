5bff6063e15b7c6b68f018bdcb592ca3b9fdee77
Willians Martins <professorwillians@gmail.com>
2022-11-08 19:32:52 -0300

    atualizacao

--- prateleira-digital/src/br/com/impacta/apresentacao/TelaCriarFilme.java
+++ prateleira-digital/src/br/com/impacta/apresentacao/TelaCriarFilme.java
@@ -1,10 +1,15 @@
 package br.com.impacta.apresentacao;
 
+import javax.swing.JFrame;
+
 public class TelaCriarFilme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame();
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
