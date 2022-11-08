e0462c5a6a42a884851a305b59ed29bcf7085eb1
Willians Martins <professorwillians@gmail.com>
2022-11-08 20:24:06 -0300

    atualizacao

--- prateleira-digital/src/br/com/impacta/apresentacao/TelaCriarFilme.java
+++ prateleira-digital/src/br/com/impacta/apresentacao/TelaCriarFilme.java
@@ -10,6 +10,8 @@ import javax.swing.JTextField;
 public class TelaCriarFilme {
 
 	public static void main(String[] args) {
+		int margem1 = 100;
+		
 		JFrame frame = new JFrame();
 		frame.setSize(800, 600);
 		
@@ -21,23 +23,23 @@ public class TelaCriarFilme {
 		botaoSalvar.setText("Salvar");
 		botaoSalvar.setSize(150, 40);
 		botaoSalvar.setLocation(550, 450);
+		frame.add(botaoSalvar);
 		
 		JLabel labelTopo = new JLabel("Gerenciamento de filmes");
 		labelTopo.setFont(new Font("Arial", Font.BOLD, 34));
 		labelTopo.setBounds(50, 5, 500, 50);
+		frame.add(labelTopo);
 		
 		//titulo
 		JLabel labelTitulo = new JLabel("Título:");
 		labelTitulo.setBounds(50, 100, 100, 30);
-		JTextField caixa1 = new JTextField(20);
-		caixa1.setBounds(100, 100, 200, 30);	
+		JTextField caixaTitulo = new JTextField(20);
+		caixaTitulo.setBounds(margem1, 100, 200, 30);	
+		frame.add(labelTitulo);
+		frame.add(caixaTitulo);
 		
 		//diretores
 		
-		frame.add(botaoSalvar);
-		frame.add(caixa1);
-		frame.add(labelTitulo);
-		frame.add(labelTopo);
 		
 		frame.setLayout(null);
 		frame.setVisible(true);
