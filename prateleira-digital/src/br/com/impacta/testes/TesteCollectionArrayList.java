2275b4b5708f23fe37187520bb3b055f0388ad74
Willians Martins <professorwillians@gmail.com>
2022-11-09 21:07:09 -0300

    atualizacao

---
+++ prateleira-digital/src/br/com/impacta/testes/TesteCollectionArrayList.java
@@ -0,0 +1,28 @@
+package br.com.impacta.testes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteCollectionArrayList {
+
+	public static void main(String[] args) {
+		List lista = new ArrayList();
+		lista.add("Willians");
+		lista.add("Luis");
+		lista.add("Luis");
+		lista.add("Rosalia");		
+		
+		
+		System.out.println(lista);	
+
+		System.out.println(lista.size());	
+		
+		lista.remove("Luis");
+		System.out.println(lista);	
+		
+		lista.clear();
+		System.out.println(lista);
+	}
+
+
+}
---
+++ prateleira-digital/src/br/com/impacta/testes/TesteCollectionHashSet.java
@@ -0,0 +1,30 @@
+package br.com.impacta.testes;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class TesteCollectionHashSet {
+
+	public static void main(String[] args) {
+		Set conjunto = new HashSet();
+		
+		conjunto.add("Willians");		
+		conjunto.add("Luis");		
+		conjunto.add("Luis");		
+		conjunto.add("Rosalia");		
+		
+		
+		System.out.println(conjunto);	
+
+		System.out.println(conjunto.size());	
+		
+		conjunto.remove("Luis");
+		System.out.println(conjunto);	
+		
+		conjunto.clear();
+		System.out.println(conjunto);
+	}
+
+
+}
