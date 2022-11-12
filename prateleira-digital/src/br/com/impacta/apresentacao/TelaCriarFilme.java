638e7f507ea8210190c8a5771c1eb64aecf054f2
Willians Martins <professorwillians@gmail.com>
2022-11-11 21:46:58 -0300

    atualizacao

--- prateleira-digital/src/br/com/impacta/apresentacao/TelaCriarFilme.java
+++ prateleira-digital/src/br/com/impacta/apresentacao/TelaCriarFilme.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Iterator;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -238,13 +239,24 @@ public class TelaCriarFilme {
 		    @Override
 		    public void valueChanged(ListSelectionEvent event) {
 	        	int linha = tabela.getSelectedRow();
-	        	if(linha>0) {
+	        	if(linha>=0 && !event.getValueIsAdjusting() ) {
 					String titulo = tabela.getValueAt(linha, 0).toString();
-					System.out.println(titulo);
 					caixaTitulo.setText(titulo);
 					
 					//objetivo = encontrar 1 filme no meio de vários
-					System.out.println(controller.galeria);
+					Filme filmeEscolhido = null;
+					
+					for (Filme filme : controller.galeria) {
+						System.out.println(filme.titulo);
+						
+						if( titulo.equals(filme.titulo) ) {
+							filmeEscolhido = filme;
+							break;
+						}
+					}
+					
+					System.out.println(filmeEscolhido);
+					
 	        	}
 		    }
 		});
