e8b18cbd747bbf95b1be07ae87f1f08dc3ae104b
Willians Martins <professorwillians@gmail.com>
2022-11-17 21:39:29 -0300

    update

--- prateleira-digital/src/br/com/impacta/apresentacao/TelaCriarFilme.java
+++ prateleira-digital/src/br/com/impacta/apresentacao/TelaCriarFilme.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -33,7 +34,7 @@ public class TelaCriarFilme {
 	static boolean ehNovo = true;
 
 	public static void main(String[] args) {
-		controller.listar();
+		List galeria = controller.listar();
 		int margem1 = 100;
 		
 		JFrame frame = new JFrame();
