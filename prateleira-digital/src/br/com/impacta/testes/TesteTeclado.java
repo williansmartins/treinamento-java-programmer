9a236298e64a2460b0d1ce5e0701c23697a13d71
Willians Martins <professorwillians@gmail.com>
2022-11-22 21:34:01 -0300

    update

--- prateleira-digital/src/br/com/impacta/testes/TesteTeclado.java
+++ prateleira-digital/src/br/com/impacta/testes/TesteTeclado.java
@@ -7,15 +7,9 @@ public class TesteTeclado {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in); 
 		
-		System.out.println("*** Insira o ano nascimento 11111***");
+		System.out.println("*** Insira o ano nascimento ***");
 		String anoString = scan.nextLine();
 		int anoInteiro = Integer.parseInt(anoString);			
-		System.out.println( "Idade = " + (2022-anoInteiro) );
-		
-		
-		System.out.println("*** Insira o ano nascimento 22222***");
-		anoString = scan.nextLine();
-		anoInteiro = Integer.parseInt(anoString);			
 		System.out.println( "Idade = " + (2022-anoInteiro) );
 	}
 
