ee5cf0cdfe1d9ebd9136433d1e12daac4552a8ca
Willians Martins <professorwillians@gmail.com>
2022-11-24 20:33:08 -0300

    update

---
+++ projeto-exemplos/src/capitulo_04/Media.java
@@ -0,0 +1,65 @@
+package capitulo_04;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class Media {
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame("Calcular Média");
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(320, 400);
+		frame.setLayout(null);
+		frame.setLocationRelativeTo(null);
+
+		JTextField caixa1 = new JTextField();
+		caixa1.setBounds(15, 10, 100, 30);
+		frame.add(caixa1);
+
+		JTextField caixa2 = new JTextField();
+		caixa2.setBounds(15, 50, 100, 30);
+		frame.add(caixa2);
+
+		JTextField caixa3 = new JTextField();
+		caixa3.setBounds(15, 90, 200, 30);
+		frame.add(caixa3);
+
+		JButton botaoMedia = new JButton("Média");
+		botaoMedia.setBounds(15, 130, 100, 30);
+		frame.add(botaoMedia);
+
+		botaoMedia.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String nota1String = caixa1.getText();
+				String nota2String = caixa2.getText();
+				System.out.println(nota1String);
+
+				double nota1Double = Double.parseDouble(nota1String);
+				double nota2Double = Double.parseDouble(nota2String);
+				double media = (nota1Double + nota2Double) / 2;
+
+				if (media < 6) {
+
+					caixa3.setText("Aluno Reprovado - " + media );
+				} else {
+
+					caixa3.setText("Aluno Aprovado - " + media);
+				}
+
+			}
+		});
+
+		frame.setVisible(true);
+
+	}
+
+}
